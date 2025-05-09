e905f956a77c4d1b8b3ba0dea602fafdf49b4dfa
pedro <142040661+Pedroceag@users.noreply.github.com>
2025-05-09 07:57:44 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -26,4 +26,43 @@ my_list.append(6)
 print("Lista após adicionar 6:", my_list)
 
 #Inserindo um elemento em posição específica:
-#- 
+#- Simtaxe: insert(índici, valor)
+#- Deslocar elementos existentes para a direita 
+my_list.insert(2, 7)
+print("Lista após inserir 7 na posição 2:", my_list)
+
+# Removando a primeira ocorrência de um elemento
+print("Último elemento", array[-1])
+
+my_list.remove(4)
+print("Lista após remover o valor 4:", my_list)
+
+#Tuplas (estruturas imutável de elementos)
+#Criando uma tupla - usa parêntese ao invés de colchetes
+my_tuple = (1, 2, 3, 4, 5)
+print("Tuplas:", my_tuple)
+
+#Acesso a elementos funciona igual ás listas 
+print("Primeiro elemento da tupla", my_tuple[0])
+print("Ùltimo elemento da tupla:", my_tuple[-1])
+
+#Loops (estruturas de repetição)
+
+#Loops for iterando sobre elemento de uma lista 
+fruits = ["maça", "banana", "morango"]
+print("Frutas na lista:")
+for fruit in fruits:
+    print(fruit)
+
+#Loop while executando enquando condição é verdadeira
+print("contagem de 0 a 4:")
+1 = 0
+while 1 < 5:
+    print(i)
+    1 += 1  #incrementa o contador 
+
+#Loop for com acesso ao índice e elemento simultaniamente usando enumerate
+print("Elemento da lista com seus índice:")
+my_list = [1, 2, 3, 4, 5]   
+for indice, elemento in enumerate(my_list):
+    print(f"indice (indice): (elemento)")     
